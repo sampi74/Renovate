c88f214a242b877014f37a00e54ec5560792567f
sampi74 <facusampieri74@gmail.com>
2024-06-05 13:20:47 -0300

    commit 5/6

---
+++ app/enums.py
@@ -0,0 +1,9 @@
+import enum
+
+
+class Talle(enum.Enum):
+    S = "S"
+    M = "M"
+    L = "L"
+    XL = "XL"
+    XXL = "XXL"
--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@
 from app import db
 from datetime import datetime
 
+from app.enums import Talle
+
 
 class RolUsuario(db.Model):
     cod_rol = db.Column(db.Integer, primary_key=True)
@@ -18,8 +20,12 @@ class Usuario(db.Model):
     nombre_usuario = db.Column(db.String(14), index=True, unique=True)
     contrasena_usuario = db.Column(db.String(20))
     email_usuario = db.Column(db.String(100), index=True, unique=True)
+    foto_usuario = db.Column(db.String(300))
     fecha_nacimiento_usuario = db.Column(db.Date)
     cod_rol = db.Column(db.Integer, db.ForeignKey('rol_usuario.cod_rol'))
+    mensajes_enviados = db.relationship('Mensaje', foreign_keys='Mensaje.cod_emisor', backref='emisor', lazy='dynamic')
+    mensajes_recibidos = db.relationship('Mensaje', foreign_keys='Mensaje.cod_receptor', backref='receptor',
+                                         lazy='dynamic')
 
 
 class Direccion(db.Model):
@@ -50,3 +56,43 @@ class Provincia(db.Model):
         self.fecha_baja_provincia = datetime.utcnow().date()
 
 
+class Publicacion(db.Model):
+    cod_publicacion = db.Column(db.Integer, primary_key=True)
+    nombre_publicacion = db.Column(db.String(100), index=True)
+    descripcion_publicacion = db.Column(db.String(300))
+    precio_publicacion = db.Column(db.Float, index=True)
+    talle_publicaion = db.Column(db.Enum(Talle))
+    color_publicacion = db.Column(db.String(20), index=True)
+    vendido = db.Column(db.Boolean, default=False)
+    foto_publicacion = db.Column(db.String(300))
+    cod_subcategoria = db.Column(db.Integer, db.ForeignKey('subcategoria.cod_subcategoria'))
+    mensajes = db.relationship('Mensaje', backref='publicacion', lazy='dynamic')
+
+
+class Subcategoria(db.Model):
+    cod_subcategoria = db.Column(db.Integer, primary_key=True)
+    nombre_subcategoria = db.Column(db.String(100), index=True, unique=True)
+    fecha_baja_subcategoria = db.Column(db.Date, default=None)
+    publicaciones = db.relationship('Publicacion', backref='subcategoria', lazy='dynamic')
+
+    def dar_de_baja(self):
+        self.fecha_baja_subcategoria = datetime.utcnow().date()
+
+
+class Categoria(db.Model):
+    cod_categoria = db.Column(db.Integer, primary_key=True)
+    nombre_categoria = db.Column(db.String(100), index=True, unique=True)
+    fecha_baja_categoria = db.Column(db.Date, default=None)
+    subcategorias = db.relationship('Subcategoria', backref='categoria', lazy='dynamic')
+
+    def dar_de_baja(self):
+        self.fecha_baja_categoria = datetime.utcnow().date()
+
+
+class Mensaje(db.Model):
+    cod_mensaje = db.Column(db.Integer, primary_key=True)
+    fecha_mensaje = db.Column(db.Date)
+    contenido_mensaje = db.Column(db.String(300))
+    cod_emisor = db.Column(db.Integer, db.ForeignKey('usuario.cod_usuario'))
+    cod_receptor = db.Column(db.Integer, db.ForeignKey('usuario.cod_usuario'))
+    cod_publicacion = db.Column(db.Integer, db.ForeignKey('publicacion.cod_publicacion'))
